7ff6da91be64d5b52aa969780a59a38136d60182
Seth Quittner <sethaugustus@gmail.com>
2016-04-04 20:40:49 -0700

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,21 @@
+#Use hashes with symbols to represent the following scenario:
+#You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
+#Represent 3 items using hashes. Each hash should have the same keys with different values.
+#Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. #JavaScript syntax {a: 123}).
+products = [
+  {
+    color: "red",
+    size: "medium",
+    price: 98.07
+  },
+  {
+    :color => "green",
+    :size => "small",
+    :price => 12.09
+  },
+  {
+    color: "blue",
+    size: "X-Large",
+    price: 23.00
+  }
+]
